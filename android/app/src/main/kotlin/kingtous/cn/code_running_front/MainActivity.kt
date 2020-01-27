dffb86a1f5937bc327666eebafda025698a25acb
Kingtous <kingtous@qq.com>
2020-01-27 18:30:31 +0800

    初始化仓库

---
+++ android/app/src/main/kotlin/kingtous/cn/code_running_front/MainActivity.kt
@@ -0,0 +1,12 @@
+package kingtous.cn.code_running_front
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
